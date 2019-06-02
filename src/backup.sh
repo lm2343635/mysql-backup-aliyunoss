da308c443e903407f1095b88ee4905f75d2f9ec8
Meng Li <lm2343635@126.com>
2019-06-03 00:16:00 +0900

    Test upload.py

--- src/backup.sh
+++ src/backup.sh
@@ -2,4 +2,9 @@ while read line;do
     eval "$line"
 done < config
 
-mysqldump -h $db_host -u $db_user -p$db_password $db_dbname > "./$db_dbname.sql"
+date=`date +%Y%m%d_%H%M%S`
+filename="${db_dbname}_${date}.sql"
+rm *.sql
+mysqldump -h $db_host -u $db_user -p$db_password $db_dbname > "./$filename"
+
+python3 upload.py $oss_accessKeyId $oss_accessKeySecret $oss_endpoint $oss_bucket $filename $filename
--- src/upload.py
+++ src/upload.py
@@ -3,12 +3,18 @@ import oss2
 
 accessKeyId = sys.argv[1]
 accessKeySecret = sys.argv[2]
+print(accessKeyId)
+print(accessKeySecret)
 auth = oss2.Auth(accessKeyId, accessKeySecret)
 
 endpoint = sys.argv[3]
 bucket = sys.argv[4]
+print(endpoint)
+print(bucket)
 bucket = oss2.Bucket(auth, endpoint, bucket)
 
 objectName = sys.argv[5]
 localFile = sys.argv[6]
+print(objectName)
+print(localFile)
 bucket.put_object_from_file(objectName, localFile)
