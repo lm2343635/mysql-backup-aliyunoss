b258afc9b91381104e644dc0109c51573eebcc97
Meng Li <lm2343635@126.com>
2019-06-07 11:30:43 +0900

    Upload zip.

--- src/backup.sh
+++ src/backup.sh
@@ -3,9 +3,13 @@ while read line;do
 done < config
 
 date=`date +%Y%m%d_%H%M%S`
-filename="${db_dbname}_${date}.sql"
-rm *.sql
-mysqldump -h $db_host -u $db_user -p$db_password $db_dbname > "./$filename"
+sql="${db_dbname}_${date}.sql"
+zip="$sql.zip"
+mysqldump -h $db_host -u $db_user -p$db_password $db_dbname > "./$sql"
+zip -r $zip $sql
 
+echo "Uploading backup file $zip to Aliyun OSS..."
+python3 upload.py -k $oss_accessKeyId -s $oss_accessKeySecret -e $oss_endpoint -b $oss_bucket -o $zip -f $zip
+echo "Uploaded!"
 
-python3 upload.py -k $oss_accessKeyId -s $oss_accessKeySecret -e $oss_endpoint -b $oss_bucket -o $filename -f $filename
+rm $sql $zip
